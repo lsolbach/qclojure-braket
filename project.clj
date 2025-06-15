47ee17d9544ad2b00546c58ef9589dc492cd4af9
Ludger Solbach <soulman@soulspace.org>
2025-06-15 15:10:27 +0200

    added S3 API

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure "1.12.1"]
                  [com.cognitect.aws/api       "0.8.741"]
                  [com.cognitect.aws/endpoints "871.2.31.23"]
+                 [com.cognitect.aws/s3        "871.2.31.23"]
                  [com.cognitect.aws/braket    "871.2.29.35"]
                  [org.soulspace/qclojure "0.1.0-SNAPSHOT"]]
   )
