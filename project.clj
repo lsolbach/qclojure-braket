9439d06eeb08a24e36261b7dc52b64f1306148f0
Ludger Solbach <soulman@soulspace.org>
2025-07-23 07:41:23 +0200

    updated aws api versions

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :description "A Clojure Backend for Amazon Braket"
   :dependencies [[org.clojure/clojure         "1.12.1"]
                  [com.cognitect.aws/api       "0.8.741"]
-                 [com.cognitect.aws/endpoints "871.2.31.23"]
-                 [com.cognitect.aws/s3        "871.2.31.23"]
+                 [com.cognitect.aws/endpoints "871.2.32.5"]
+                 [com.cognitect.aws/s3        "871.2.32.2"]
                  [com.cognitect.aws/braket    "871.2.29.35"]
-                 [com.cognitect.aws/pricing   "871.2.31.23"]
-                 [com.cognitect.aws/iam       "871.2.30.22"]
+                 [com.cognitect.aws/pricing   "871.2.32.22"]
+                 [com.cognitect.aws/iam       "871.2.32.2"]
                  [org.soulspace/qclojure      "0.8.0"]])
