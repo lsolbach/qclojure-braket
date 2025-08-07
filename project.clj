87b2af56975206298136852efbf01262cb792fbc
Ludger Solbach <soulman@soulspace.org>
2025-08-07 09:44:22 +0200

    updated dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject qclojure-braket "0.1.0-SNAPSHOT"
+(defproject org.soulspace/qclojure-braket "0.1.0-SNAPSHOT"
   :description "A Clojure Backend for Amazon Braket"
   :dependencies [[org.clojure/clojure         "1.12.1"]
-                 [com.cognitect.aws/api       "0.8.741"]
-                 [com.cognitect.aws/endpoints "871.2.32.5"]
+                 [com.cognitect.aws/api       "0.8.762"]
+                 [com.cognitect.aws/endpoints "871.2.32.15"]
                  [com.cognitect.aws/s3        "871.2.32.2"]
                  [com.cognitect.aws/braket    "871.2.29.35"]
                  [com.cognitect.aws/pricing   "871.2.32.22"]
                  [com.cognitect.aws/iam       "871.2.32.2"]
-                 [org.soulspace/qclojure      "0.8.0"]])
+                 [org.soulspace/qclojure      "0.10.0"]])
