10e7f8727c0b3d69e4b891dc85f1da0f0c151bf7
Ludger Solbach <soulman@soulspace.org>
2025-11-16 11:57:34 +0100

    add camel-snake-kebab and update AWS deps

--- project.clj
+++ project.clj
@@ -2,15 +2,16 @@
   :description "Provides an Amazon Braket Backend for QClojure"
   :license {:name "Eclipse Public License 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure         "1.12.3"]
-                 [com.cognitect.aws/api       "0.8.774"]
-                 [com.cognitect.aws/endpoints "871.2.35.1"]
-                 [com.cognitect.aws/s3        "871.2.33.5"]
-                 [com.cognitect.aws/braket    "871.2.32.25"]
-                 [com.cognitect.aws/pricing   "871.2.32.2"]
-                 [com.cognitect.aws/iam       "871.2.32.2"]
-                 [zprint/zprint               "1.3.0"]
-                 [org.soulspace/qclojure      "0.24.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure                 "1.12.3"]
+                 [com.cognitect.aws/api               "0.8.774"]
+                 [com.cognitect.aws/endpoints         "871.2.38.3"]
+                 [com.cognitect.aws/s3                "871.2.37.4"]
+                 [com.cognitect.aws/braket            "871.2.38.3"]
+                 [com.cognitect.aws/pricing           "871.2.32.2"]
+                 [com.cognitect.aws/iam               "871.2.38.3"]
+                 [zprint/zprint                       "1.3.0"]
+                 [camel-snake-kebab/camel-snake-kebab "0.4.3"]
+                 [org.soulspace/qclojure              "0.24.0-SNAPSHOT"]]
 
   :scm {:name "git" :url "https://github.com/lsolbach/qclojure-braket"}
   :deploy-repositories [["clojars" {:sign-releases false :url "https://clojars.org/repo"}]])
